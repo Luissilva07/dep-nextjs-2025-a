145f4ba39c5b55cc848cfcd7f9302ff8c5279761
Lsilva Dev <101373147+Luissilva07@users.noreply.github.com>
2025-09-01 11:19:25 +0000

    Aula 01/09/2025

---
+++ components/main-screen.tsx
@@ -0,0 +1,10 @@
+const MainScreen = () => {
+    return (
+        <main> 
+            <h1>Exemplo de Aplicação Next</h1>
+            <p>Teste de Implantação</p>
+        </main>
+    );
+}
+
+export default MainScreen;
